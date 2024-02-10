7899d60ee7ecc8cc9c55e5dd0af48468b129604b
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-02-10 10:20:15 +0530

    Review

--- src/Utils/Reviews.js
+++ src/Utils/Reviews.js
@@ -1,11 +1,29 @@
 const Reviews = [
   {
     id: 1,
+    name: "Anubhav Raj",
+    bio: "Full Stack Developer",
+    img: "https://res.cloudinary.com/ddjk9ccx5/image/upload/v1707540418/Projects/s4gwiet6ajxrydiiqdwh.jpg",
+    description:
+      "I was finding someone to help me to build my Grocery website for my store. He made me an E-commerce Store. I am satisfied with his work.",
+    rating: 5.0,
+  },
+  {
+    id: 2,
     name: "Snehit Garg",
     bio: "ML Developer",
     img: "https://res.cloudinary.com/ddjk9ccx5/image/upload/v1707379971/Projects/sg.jpg",
     description:
       "I am very impressed to work with him. He is a very good and skilled person. I want to work with him again.",
+    rating: 5.0,
+  },
+  {
+    id: 3,
+    name: "Aditya Tyagi",
+    bio: "Front-end Developer",
+    img: "https://res.cloudinary.com/ddjk9ccx5/image/upload/v1707539438/Projects/c0fi3kps0ixu1apbbyr1.jpg",
+    description:
+      "He is my partner. We work together. When I fall into an error or any other problem he support me always.",
     rating: 5.0,
   },
 ];

db18c383c248f2e860bb5ba73fe0d751785b510d
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-03-16 12:01:12 +0530

    update skill

--- src/components/molecules/MySkill/index.js
+++ src/components/molecules/MySkill/index.js
@@ -27,21 +27,34 @@ const MySkill = () => {
       icon: <SiJavascript className="text-yellow-500 rounded" />,
     },
     { title: "NodeJS", icon: <FaNodeJs className="text-green-600" /> },
+    { title: "TypeScript", icon: <SiTypescript className="text-blue-600" /> },
+    { title: "C++", icon: <SiCplusplus className="text-blue-800" /> },
+    { title: "Python", icon: <SiPython className="text-yellow-500" /> },
   ];
+
   const frameworks = [
     { title: "React", icon: <FaReact className="text-cyan-400" /> },
     { title: "Bootstrap", icon: <FaBootstrap className="text-indigo-600" /> },
     { title: "Tailwind", icon: <SiTailwindcss className="text-cyan-400" /> },
     { title: "ExpressJS", icon: <SiExpress className="text-neutral" /> },
+    { title: "WordPress", icon: <SiWordpress className="text-blue-400" /> },
   ];
+
   const tools = [
     { title: "MongoDB", icon: <SiMongodb className="text-green-500" /> },
     { title: "GitHub", icon: <FaGithub className="text-black" /> },
-
     {
       title: "Photoshop",
       icon: <SiAdobephotoshop className="text-cyan-800" />,
     },
+    { title: "NumPy", icon: <SiNumpy className="text-yellow-500" /> },
+    { title: "Pandas", icon: <SiPandas className="text-blue-500" /> },
+    { title: "API", icon: <SiApiplatform className="text-purple-600" /> },
+    { title: "SQL", icon: <SiMysql className="text-blue-500" /> },
+    { title: "Design Patterns", icon: <SiDesignpatterns className="text-green-600" /> },
+    { title: "DSA", icon: <SiDatastructures className="text-red-600" /> },
+    { title: "DBMS", icon: <SiMysql className="text-blue-500" /> },
+    { title: "OS", icon: <SiOperatingSystem className="text-gray-600" /> },
   ];
 
   const defaultOptions = {
@@ -52,13 +65,13 @@ const MySkill = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+
   return (
     <div className="pt-24">
       <h1 className="text-4xl font-semibold drop-shadow-md text-center mb-8">
         My <span className="text-primary">Skills</span>
       </h1>
       <div className="flex flex-col md:flex-row items-center justify-evenly my-8">
-
         <div>
           <h2 className="text-cyan-500 text-xl font-semibold">Languages:</h2>
           <div className="flex flex-wrap mb-4">

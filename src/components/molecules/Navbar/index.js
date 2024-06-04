24a60a2b321669965d83d7ff59ec658514e2e153
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-06-04 22:00:23 +0530

    Updated

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -83,7 +83,7 @@ export default function Navbar() {
 
             <a
               className="inline-block ml-4"
-              href="https://drive.google.com/file/d/1Zh-2c3VLIulTIW-rOvkPjwDmKr7y7AuA/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1NaHkVSOj101Kd1Lw8e9344r9BTQQuHT0/view?usp=sharing?usp=drive_link"
               target="blank"
             >
               <PrimaryBtn>
@@ -131,7 +131,7 @@ export default function Navbar() {
                 <li className="text-center m-4">
                   <a
                     className="inline-block w-full"
-                    href="https://drive.google.com/file/d/1Zh-2c3VLIulTIW-rOvkPjwDmKr7y7AuA/view?usp=drive_link"
+                    href="https://drive.google.com/file/d/1NaHkVSOj101Kd1Lw8e9344r9BTQQuHT0/view?usp=sharing?usp=drive_link"
                     target="blank"
                   >
                     <button className="primary-button w-full text-white">
